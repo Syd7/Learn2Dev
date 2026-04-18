d45a58db9628d4856bab9410de30477d9d738067
erennuevo <lib.nuevo@student.ateneo.edu>
2026-04-18 22:30:59 +0800

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: API_URL,
 });
 
 axiosInstance.interceptors.request.use((config) => {
